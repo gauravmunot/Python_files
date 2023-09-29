add7917e26713b0c2d1a18a6a5ab8cdf65cf9aaf
gauravmunot <gauravmunot1591@gmail.com>
2023-09-29 18:05:51 +0530

    Pizz added

--- hungry.py
+++ hungry.py
@@ -3,5 +3,7 @@ print("I am hungry")
 hungry = input("Are you hungry ? ")
 if hungry == "yes":
     print("Eat some samosa")
+    print("Lets have Pizza also")
+
 else:
     print("take rest and sleep for some time")
