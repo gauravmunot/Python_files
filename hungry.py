68388b361aaff222aa85999b4002259b8bb3f333
gauravmunot <gauravmunot1591@gmail.com>
2023-09-29 18:19:02 +0530

    Burger 2 added

--- hungry.py
+++ hungry.py
@@ -3,8 +3,11 @@ print("I am hungry")
 hungry = input("Are you hungry ? ")
 if hungry == "yes":
     print("Eat some samosa")
+<<<<<<< HEAD
     print("Lets have Pizza also")
     print("Eat Burger")
 
+=======
+>>>>>>> parent of add7917 (Pizz added)
 else:
     print("take rest and sleep for some time")
